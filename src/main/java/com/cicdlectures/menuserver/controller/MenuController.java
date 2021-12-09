754084d79c9b0ab56a53b727bd479b7d0f0e208b
ThomasDBM <thomas.debeaumont@free.fr>
2021-12-09 12:54:39 +0000

    Add delete menu method

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 
+import com.cicdlectures.menuserver.controller.MenuNotFoundException;
 import com.cicdlectures.menuserver.dto.MenuDto;
 import com.cicdlectures.menuserver.service.CreateMenuService;
 import com.cicdlectures.menuserver.service.ListMenuService;
+import com.cicdlectures.menuserver.repository.MenuRepository;
 
 @RestController
 public class MenuController {
@@ -21,10 +25,13 @@ public class MenuController {
 
   private final ListMenuService listMenuService;
 
+  private final MenuRepository menuRepository;
+
   @Autowired
-  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService) {
+  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService, MenuRepository menuRepository) {
     this.createMenuService = createMenuService;
     this.listMenuService = listMenuService;
+    this.menuRepository = menuRepository;
   }
 
   @GetMapping(path = "/menus", produces = "application/json")
@@ -36,5 +43,14 @@ public class MenuController {
   @ResponseStatus(HttpStatus.CREATED)
   public MenuDto createMenu(@RequestBody MenuDto menu) {
     return createMenuService.createMenu(menu);
+  }
+
+  @DeleteMapping("/menus/{id}")
+  public void deletemenus(@PathVariable Long id) {
+    if (menuRepository.existsById(id)){
+      menuRepository.deleteById(id);
+    } else {
+      throw new MenuNotFoundException(id);
+    }
   }
 }
